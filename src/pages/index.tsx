347ccc4e6102bafada8a31d1dcfeaf9d29bb0d6a
Aphiwit1 <aphiwit.boon@gmail.com>
2023-07-24 17:34:33 +0700

    update: time and any

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Navbar from '@/features/Navbar/Navbar';
 import StarWarsListV2 from '@/features/StarWarsListV2/StarWarsListV2';
 import { GraphQLClient } from 'graphql-request';
 import { getPostsQuery } from './api/graphqlQueries';
-import { filmsProp } from '@/features/StarWarsListV2/interface';
+import { Film, filmsProp } from '@/features/StarWarsListV2/interface';
 
 
 // Type แปลกๆ อย่าลืมมาดูด้วยนะ 
@@ -17,10 +17,17 @@ export default function Home({ films }:filmsProp) {
   )
 }
 
+interface GraphQLResponse {
+  allFilms: {
+    films: Film[];
+  };
+}
+
 export async function getStaticProps() {
   const graphQLClient = new GraphQLClient('https://swapi-graphql.netlify.app/.netlify/functions/index');
   try {
-    const data:any = await graphQLClient.request(getPostsQuery);
+    const data:GraphQLResponse = await graphQLClient.request(getPostsQuery);
+    console.log('data >>>>', data)
     const films = data.allFilms.films;
 
     return { props: { films } };
