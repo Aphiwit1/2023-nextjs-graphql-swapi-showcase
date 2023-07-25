7cffd4101a141d139b04315ea08cb2015af52162
Aphiwit1 <aphiwit.boon@gmail.com>
2023-07-25 16:06:46 +0700

    update: router starwar-films

--- src/features/Navbar/Navbar.tsx
+++ src/features/Navbar/Navbar.tsx
@@ -1,29 +1,36 @@
 import { WithNavbar } from "./WithNavbar";
-import Image from "next/image";
+
+import Link from 'next/link'
 
 const Navbar = () => (
   <>
     <section
-      className="flex flex-col min-h-screen bg-gradient-to-r text-white bg-center bg-cover bg-fixed "
+      className="flex flex-col min-h-[80vh] bg-gradient-to-r text-white bg-center bg-cover bg-fixed "
       style={{
         backgroundImage: `url("https://images.unsplash.com/photo-1623476408624-721c9185d569?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1738&q=80")`,
       }}
     >
       <div className="flex flex-row justify-center bg-blue-950 p-2 text-center">
-        This project created by NextJS (Updated: 14.38)
+        This project created by NextJS (Updated: 16.06)
       </div>
       {/* STAR WARS Logo */}
-      <div className="flex items-center h-20 border-b-[1px] border-yellow-500">
-        <div className="mx-auto relative px-5 max-w-screen-xl w-full flex items-center  justify-center">
-          <div className="flex justify-start p-3 ">
-            <img
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Star_Wars_Logo..png/640px-Star_Wars_Logo..png"
-              width="100px"
-              height="auto"
-            ></img>
-
-          </div>
+      <div className="flex items-center justify-between h-20 border-b-[1px] border-yellow-500 px-10">
+        <div>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Star_Wars_Logo..png/640px-Star_Wars_Logo..png"
+            width="100px"
+            height="auto"
+          ></img>
         </div>
+
+        <div>
+        <Link href="/" className="font-medium text-white hover:text-yellow-500">Home</Link>
+        <Link href="starwar-films" className="ml-5 font-medium text-white hover:text-yellow-500">Films</Link>
+
+        <Link href="starwar-films" className="ml-5 font-medium text-white hover:text-yellow-500">People</Link>
+        </div>
+
+        <div>Dark Mode</div>
       </div>
 
       {/*  Heading Caption */}
---
+++ src/pages/starwar-films.tsx
@@ -0,0 +1,31 @@
+import StarWarsList from '@/features/StarWar/StarWarsList/StarWarsList'
+import { GraphQLClient } from 'graphql-request'
+import React from 'react'
+import { getPostsQuery } from './api/graphqlQueries'
+import { Film } from '@/features/StarWar/StarWarsList/interface'
+import Navbar from '@/features/Navbar/Navbar'
+
+const starwarFilm = ({films}: any) => {
+  return (
+    <>
+        <Navbar />
+        <StarWarsList films={films} />
+    </>
+  )
+}
+export default starwarFilm
+
+export async function getStaticProps() {
+  const graphQLClient = new GraphQLClient('https://swapi-graphql.netlify.app/.netlify/functions/index');
+
+  try {
+    const data: { allFilms: { films: Film[] } } = await graphQLClient.request(getPostsQuery);
+    const films = data.allFilms.films;
+    console.log(">>", films)
+
+    return { props: { films } };
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return { props: { films: [] } };
+  }
+}
